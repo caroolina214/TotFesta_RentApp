0192ba7cf7f59ee4a0986f0f38d6b0a9a4c989ae
Carolina <caroolina2.14@gmail.com>
2026-06-03 11:30:22 +0200

    Add color palette, themes and dark/light mode

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,35 @@
 import { GluestackUIProvider } from '@/src/components/ui/gluestack-ui-provider';
 import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { useColorScheme } from 'react-native';
+import { SplashScreen } from 'expo-router';
+import { useEffect } from 'react';
 import '@/global.css';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+    const colorScheme = useColorScheme();
+
+    const [fontsLoaded] = useFonts({
+        'FuzzyBubbles': require('@/assets/fonts/FuzzyBubbles-Regular.ttf'),
+        'FuzzyBubbles-Bold': require('@/assets/fonts/FuzzyBubbles-Bold.ttf'),
+        'SchibstedGrotesk': require('@/assets/fonts/SchibstedGrotesk-VariableFont_wght.ttf'),
+        'SchibstedGrotesk-Italic': require('@/assets/fonts/SchibstedGrotesk-Italic-VariableFont_wght.ttf'),
+        'RobotoMono': require('@/assets/fonts/RobotoMono-VariableFont_wght.ttf'),
+        'RobotoMono-Italic': require('@/assets/fonts/RobotoMono-Italic-VariableFont_wght.ttf'),
+    });
+
+    useEffect(() => {
+        if (fontsLoaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) return null;
+
     return (
-        <GluestackUIProvider mode="light">
+        <GluestackUIProvider mode={colorScheme === 'dark' ? 'dark' : 'light'}>
             <Stack screenOptions={{ headerShown: false }} />
         </GluestackUIProvider>
     );
---
+++ src/constants/colors.ts
@@ -0,0 +1,27 @@
+export const AppColors = {
+    // Principals
+    Morat: '#6E2585',
+    Fucsia: '#EC008C',
+    Aqua: '#00AEC7',
+    Groc: '#E9FF91',
+    Verd: '#8CF981',
+
+    // Clars
+    MoratClar: '#E9D3F0',
+    FucsiaClar: '#FFDDF4',
+    AquaClar: '#E2FAFE',
+    GrocClar: '#FDFFD4',
+    VerdClar: '#DBFFD6',
+
+    // Obscurs
+    MoratObscur: '#250D2D',
+    FucsiaObscur: '#5A0036',
+    AquaObscur: '#004853',
+    GrocObscur: '#646C3E',
+    VerdObscur: '#3A6836',
+
+    // Base
+    BaseClar: '#F5F5F5',
+    BaseMig: '#9D9D9D',
+    BaseObscur: '#141414',
+} as const;
